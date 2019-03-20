4e75eeaf46489a0b1e5878c121fb11f4081b5b89
Sacit <sacit.kuheylan@yandex.com>
2019-03-20 13:44:10 +0300

    User Class and User Details

---
+++ src/Project_Classes/Users.java
@@ -0,0 +1,20 @@
+package Project_Classes;
+
+import java.security.PublicKey;
+
+public class Users {
+
+    public String Email;
+    public String Name;
+    public String Surname;
+    public String Password;
+    public String PhoneNumber;
+    public String TCNumber;
+    public String ManagerCode;
+    public String ApartmentNumber;
+    public boolean isAdmin;
+
+    public Users() {
+
+    }
+}
